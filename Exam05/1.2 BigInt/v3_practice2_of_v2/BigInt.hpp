a8a8f80835ff602abec78a78062435a6ff5a2221
Rado Andriamahakajy <94129893+rkajy@users.noreply.github.com>
2026-09-25 20:32:43 +0000

    Add compiled object file for BigInt project
    This commit introduces the compiled object file `main.o` for the BigInt project located in the `Exam05/1.2 BigInt/v3_practice2_of_v2` directory. The object file contains the compiled code necessary for the BigInt functionality, enabling further development and testing of the project.

--- Exam05/1.2 BigInt/v3_practice2_of_v2/BigInt.hpp
+++ Exam05/1.2 BigInt/v3_practice2_of_v2/BigInt.hpp
@@ -18,54 +18,53 @@
 # include <algorithm>
 # include <string>
 
-# creer un alias
-# a essayer pour alleger l'ecriture
-# la deplacer dans la classe si tu veux la limiter juste a la classe
-# using String = std::string;
+// creer un alias
+// a essayer pour alleger l'ecriture
+// la deplacer dans la classe si tu veux la limiter juste a la classe
+// using String = std::string;
 
 class BigInt
 {
 private:
-	using String	= std::String;
-	using Size		= std::string::size_type;
-	using UInt		= unsigned int;
-	Ostream			= std::ostream;
+	typedef std::string				String;
+	typedef std::string::size_type	Size;
+	typedef unsigned int			UInt;
 
-	std::string	_value;
-	std::string	addStrings(const std::string & s1, const std::string & s2) const;
+	String	_value;
+	String	addStrings(const String &s1, const String &s2) const;
 	void	removeZeros();
 
 public:
 	BigInt();
-	BigInt(unsigned int n);
-	BigInt(const std::string & num);
-	BigInt(const BigInt & src);
+	BigInt(UInt n);
+	BigInt(const String &num);
+	BigInt(const BigInt &src);
 	~BigInt();
-	BigInt &		operator=(const BigInt & rhs);
+	BigInt&			operator=(const 	BigInt &rhs);
 
-	std::string		getVal() const;
-	unsigned int	toInt(const std::string & str) const;
+	String			getVal() const;
+	UInt			toInt(const 		String &str) const;
 
-	BigInt			operator+(const BigInt & rhs) const;
-	BigInt &		operator+=(const BigInt & rhs);
+	BigInt			operator+(const 	BigInt &rhs) const;
+	BigInt&			operator+=(const 	BigInt &rhs);
 
 	BigInt			operator++(int);
-	BigInt &		operator++();
+	BigInt&			operator++();
 
-	bool			operator==(const BigInt & rhs) const;
-	bool			operator!=(const BigInt & rhs) const;
-	bool			operator>(const BigInt & rhs) const;
-	bool			operator>=(const BigInt & rhs) const;
-	bool			operator<(const BigInt & rhs) const;
-	bool			operator<=(const BigInt & rhs) const;
+	bool			operator==(const	BigInt &rhs) const;
+	bool			operator!=(const	BigInt &rhs) const;
+	bool			operator>(const 	BigInt &rhs) const;
+	bool			operator>=(const	BigInt &rhs) const;
+	bool			operator<(const		BigInt &rhs) const;
+	bool			operator<=(const	BigInt &rhs) const;
 
-	BigInt			operator<<(const BigInt & shift) const;
-	BigInt			operator>>(const BigInt & shift) const;
+	BigInt			operator<<(const 	BigInt &shift) const;
+	BigInt			operator>>(const 	BigInt &shift) const;
 
-	BigInt &		operator<<=(const BigInt & shift);
-	BigInt &		operator>>=(const BigInt & shift);
+	BigInt&			operator<<=(const 	BigInt &shift);
+	BigInt&			operator>>=(const 	BigInt &shift);
 };
 
-std::ostream &		operator<<(std::ostream & o, const BigInt & obj);
+std::ostream&		operator<<(std::ostream &o, const BigInt &obj);
 
 #endif
